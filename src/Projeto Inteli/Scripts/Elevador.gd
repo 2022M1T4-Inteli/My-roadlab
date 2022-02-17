0d4272d623f7f604f21ab66ad2ac7b43b394a824
Lederback <renatosmachado2@gmail.com>
2022-02-17 14:59:08 -0300

    Update Elevador.gd

--- src/Projeto Inteli/Scripts/Elevador.gd
+++ src/Projeto Inteli/Scripts/Elevador.gd
@@ -1,17 +1,21 @@
 extends Area2D
 
+#Declaração de variaveis
 onready var animationTree = $AnimationTree
 onready var stateMachine = animationTree.get("parameters/playback")
 
 func _ready():
+	#Definindo o que deve ser visivel inicialmente
 	$Button.visible = false
 
 
 func _on_Elevador_body_entered(body):
+	#checando se o jogador entrou na área e alterando animação
 	stateMachine.travel("Abrir")
 	$Button.visible = true
 
 
 func _on_Elevador_body_exited(body):
+	#checando se o jogador saiu da área e alterando animação
 	stateMachine.travel("Idle")
 	$Button.visible = false
