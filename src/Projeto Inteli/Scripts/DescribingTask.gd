c889e69b9637cbeee1c583cefc50aee69f79b1b4
jacksonwsaguiar <99604226+jacksonwsaguiar@users.noreply.github.com>
2022-03-18 22:48:37 -0300

    add the comments to the code

--- src/Projeto Inteli/Scripts/DescribingTask.gd
+++ src/Projeto Inteli/Scripts/DescribingTask.gd
@@ -1,23 +1,27 @@
 extends Node2D
 
-
+#arrayde string contendo a descrição das dicas que usuario deve seguir para completar os desafios
 var tips = [
 "A codificação contem numeros primos e as letras nao podem se repetir",
 "Sequência codifidicada contendo so numeros pares e possui apenas consoantes.",
 "Letras em ordem alfabetica com numeros na ordem inversa, sequencial, sem repertir letra ou numero.",
 "Sequencia que para cada letra, os numeros nao podem se repetir, apenas numeros pares e depois para cada letra numeros impares, nesses passos. (3 conjuntos de letras e numeros impares e pares)"
 ]
+#variavel que recebera todas escolhas de codigo do usario
 var code: String = ""
+#vaarivel para manipular o array de dicas, inicia em -1 para que ao começar seja incrementado para 1
 var challenge: int = -1
+#tentativas
 var attempts: int = 2
 var hits: int = 0
 
 func _ready():
+	#inicia o array
 	nextChallenge()
 	for button in get_tree().get_nodes_in_group("keyboard_group"):
 		button.connect("pressed", self, "_button_pressed", [button])
 
-
+#ação para concatenar os codigos escolhidos
 func _button_pressed(button):
 	if code=="":
 		$label_reponse.text=""
@@ -27,7 +31,7 @@ func _button_pressed(button):
 	$label_reponse.text+=" "+button.name
 	print(code)
 
-
+#função para submeter e validar se o codigo digitado estar correto
 func _on_button_submit_pressed():
 	if code != "":
 		var success: Color= Color("#2ecc71")
@@ -64,7 +68,7 @@ func _on_button_submit_pressed():
 	else:
 		pass
 
-
+#fim da task
 func taskFineshed():
 	if hits >= 3:
 		if !Global.describingTaskComplete:
@@ -76,7 +80,7 @@ func taskFineshed():
 	else:
 		$TaskRetry.visible = true
 
-
+#tratamento para as tentativas
 func testAttempts(status):
 	var red :Color = Color("#f22613")
 	code = ""
@@ -107,7 +111,7 @@ func nextChallenge():
 		$label_reponse.text="Voce tem " + String(attempts) + " tentativas"
 		$label_tip.text = tips[challenge]
 
-
+#verifica se um numero é prime
 func isPrime(num):
 	if num> 1:  
 		for n in range(2,num):  
@@ -117,7 +121,7 @@ func isPrime(num):
 	else:
 		return false
 
-
+#verifica se o primeiro desafio estar correto
 func checkFirstRule():
 	var codes = code.split("-")
 	print(codes)
@@ -131,6 +135,7 @@ func checkFirstRule():
 	return true
 
 
+#verifica se o segundo desafio estar correto
 func checkSecondRule():
 	var codes = code.split("-")
 	
@@ -145,6 +150,7 @@ func checkSecondRule():
 	return true
 
 
+#função que verifica a ordem de um array conforme os atributos
 func isInOrder(arr: Array, type: String):
 	var nonSorted = arr
 	nonSorted = PoolStringArray(nonSorted).join("")
@@ -156,6 +162,7 @@ func isInOrder(arr: Array, type: String):
 	return nonSorted==PoolStringArray(arr).join("")
 
 
+#verifica se o terceiro desafio estar correto
 func checkThirdRule():
 	var codes = code.split("-")
 	var alphaCode = []
@@ -174,6 +181,7 @@ func checkThirdRule():
 	return true
 
 
+#verifica se a quarta desafio estar correto
 func checkFourthRule():
 	var codes: Array = code.split("-")
 	print(codes.slice(0,2))
@@ -197,13 +205,13 @@ func checkFourthRule():
 func _on_Button_pressed():
 	queue_free()
 
-
+#retornar ação ao clicar no botão close
 func _on_Close_Button_pressed():
 	get_node("/root/Level 1/Player").canWalk = true
 	get_node("/root/Level 1/Cenário 2").taskOcorring = false
 	queue_free()
 
-
+#função para tentar de novo
 func _on_TryAgainButton_pressed():
 	var task = Global.taskDescribing.instance()
 	get_node("/root/Level 1/TasksSpawn").add_child(task)
