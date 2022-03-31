e04b2c1ee7a967d6e3c7d914be2745dbcfe9ace5
Lederback <renatosmachado2@gmail.com>
2022-03-31 16:30:24 -0300

    Correção de erros

--- src/Projeto Inteli/Scripts/ComputerTask.gd
+++ src/Projeto Inteli/Scripts/ComputerTask.gd
@@ -35,30 +35,36 @@ func _StartingExplanationScreen():
 	$Explanation/ExplanationPopup.visible = true
 
 
+func _ChanginExplanation(var frame, var title, var description):
+	$Explanation/ExplanationPopup.frame = frame
+	$Explanation/ExplanationPopup/TitleLabel.text = title
+	$Explanation/ExplanationPopup/DescriptionLabel.text = description
+
+
 # Changing sprite frame according the button clicked
 func _on_ButtonProcessador_pressed():
 	_StartingExplanationScreen()
-	$Explanation/ExplanationPopup.frame = 0
+	_ChanginExplanation(0, "Processador", "O processador, também conhecido como CPU é a unidade\n central de processamento de um computador, ele funciona\n como o cérebro do computador.")
 
 
 func _on_ButtonPlacaDeVideo_pressed():
 	_StartingExplanationScreen()
-	$Explanation/ExplanationPopup.frame = 2
+	_ChanginExplanation(2, "Placa de Video", "A placa de video tem como função básica exibir imagens\n na tela, mas também é utilizada para rodar softwares que\n demandam grande processamento visual.")
 
 
 func _on_ButtonHD_pressed():
 	_StartingExplanationScreen()
-	$Explanation/ExplanationPopup.frame = 3
+	_ChanginExplanation(3, "HD", "\nO HD é um dispositivo de armazenamento de dados mais\n utilizado nos computadores. Esse tipo de equipamento guarda\n desde os seus arquivos pessoais até informações utilizadas\n exclusivamente pelo sistema operacional.")
 
 
 func _on_ButtomRAM_pressed():
 	_StartingExplanationScreen()
-	$Explanation/ExplanationPopup.frame = 1
+	_ChanginExplanation(1, "RAM", "A memória RAM é responsável pelo armazenamento de\n informações necessárias para a execução de aplicativos\n em uso e para ofuncionamento do próprio sistema\n operacional. Essa peça, inclusive, facilita o trabalho do\n processador que pode acessar os dados essenciais mais\n rapidamente.")
 
 
 func _on_ButtonFonte_pressed():
 	_StartingExplanationScreen()
-	$Explanation/ExplanationPopup.frame = 4
+	_ChanginExplanation(4, "Fonte", "A fonte tem a função de fornecer energia as demais peças do\n computador, garantindo que voltagem fornecida seja uma\n suportado pelos outros componentes.")
 
 
 # Function to activate and deactivate computer components script
--- src/Projeto Inteli/Scripts/HudEnergia.gd
+++ src/Projeto Inteli/Scripts/HudEnergia.gd
@@ -1,10 +1,12 @@
 # Made by Renato Machado
 extends Sprite
 
+# Declaring variables
 var player = false
 
 
 func _process(delta):
+	# Changing sprite frame according the amount of energy
 	match Global.energy:
 		3:
 			self.frame = 0
@@ -15,29 +17,33 @@ func _process(delta):
 		0:
 			self.frame = 3
 	
+	# Openimg coffee shop interface
 	if player && Input.is_action_just_pressed("ui_E"):
 		$Control.visible = true
 		get_node("../Player").canWalk = false
 
 
+# Checkingp if the player is in area
 func _on_Area2D_body_entered(body):
 	player = true
 	$Area2D/Sprite.frame = 1
 	$Area2D/Button.visible = true
 
-
+# Checking is the player isn't in area
 func _on_Area2D_body_exited(body):
 	player = false
 	$Area2D/Sprite.frame = 0
 	$Area2D/Button.visible = false
 
 
+# Buying coffee
 func _on_Button_pressed():
 	if Global.energy < 3:
 		Global.pontos -= 50
 		Global.energy = 3
 
 
+# Closing coffee shop interface
 func _on_CloseButton_pressed():
 	$Control.visible = false
 	get_node("../Player").canWalk = true
