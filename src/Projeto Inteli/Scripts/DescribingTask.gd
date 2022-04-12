82c5f1ad38d9cae073a05358a795c5f820025d02
Lederback <renatosmachado2@gmail.com>
2022-04-12 16:27:26 -0300

    Adição de comentários
    Adição de comentário no script do minijogo de ciência da computação.

--- src/Projeto Inteli/Scripts/DescribingTask.gd
+++ src/Projeto Inteli/Scripts/DescribingTask.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+# Declaring variables
 var tips = [
 "Combine três números primos com letras diferentes entre si. Atenção: não considere o número um como um número primo.",
 "Combine três números pares com consoantes diferentes entre si.",
@@ -13,11 +13,13 @@ var attempts: int = 2
 var hits: int = 0
 
 func _ready():
+	# Starting first challenge
 	nextChallenge()
 	for button in get_tree().get_nodes_in_group("keyboard_group"):
 		button.connect("pressed", self, "_button_pressed", [button])
 
 
+# Checking witch button is pressed
 func _button_pressed(button):
 	if code=="":
 		$label_reponse.text=""
@@ -28,6 +30,7 @@ func _button_pressed(button):
 	print(code)
 
 
+# Checking if the challenge is correct
 func _on_button_submit_pressed():
 	if code != "":
 		var success: Color= Color("#2ecc71")
@@ -65,6 +68,7 @@ func _on_button_submit_pressed():
 		pass
 
 
+# Adding points and removing energy on task fineshed
 func taskFineshed():
 	if hits >= 3:
 		if !Global.describingTaskComplete:
@@ -78,6 +82,7 @@ func taskFineshed():
 		$TaskRetry.visible = true
 
 
+# Checking task answer and changing the collors according
 func testAttempts(status):
 	var red :Color = Color("#f22613")
 	code = ""
@@ -100,6 +105,7 @@ func testAttempts(status):
 			taskFineshed()
 
 
+# Changing challenge
 func nextChallenge():
 	if challenge < 3:
 		attempts = 2
@@ -109,6 +115,7 @@ func nextChallenge():
 		$label_tip.text = tips[challenge]
 
 
+# Checking if the number are prime or not
 func isPrime(num):
 	if num> 1:  
 		for n in range(2,num):  
@@ -119,6 +126,7 @@ func isPrime(num):
 		return false
 
 
+# Checking first challenge parameters
 func checkFirstRule():
 	var codes = code.split("-")
 	print(codes)
@@ -132,6 +140,7 @@ func checkFirstRule():
 	return true
 
 
+# Checking second challenge parameters
 func checkSecondRule():
 	var codes = code.split("-")
 	
@@ -146,6 +155,7 @@ func checkSecondRule():
 	return true
 
 
+# Checking third challenge parameters
 func isInOrder(arr: Array, type: String):
 	var nonSorted = arr
 	nonSorted = PoolStringArray(nonSorted).join("")
@@ -157,6 +167,7 @@ func isInOrder(arr: Array, type: String):
 	return nonSorted==PoolStringArray(arr).join("")
 
 
+# Checking first challenge parameters
 func checkThirdRule():
 	var codes = code.split("-")
 	var alphaCode = []
@@ -175,6 +186,7 @@ func checkThirdRule():
 	return true
 
 
+# Checking fourth challenge parameters
 func checkFourthRule():
 	var codes: Array = code.split("-")
 	print(codes.slice(0,2))
@@ -200,23 +212,27 @@ func _on_Button_pressed():
 	$TaskFineshed.visible = false
 
 
+# Closing task
 func _on_Close_Button_pressed():
 	get_node("/root/Level 1/Player").canWalk = true
 	get_node("/root/Level 1/Cenário 2").taskOcorring = false
 	queue_free()
 
 
+# Restarting task
 func _on_TryAgainButton_pressed():
 	var task = Global.taskDescribing.instance()
 	get_node("/root/Level 1/TasksSpawn").add_child(task)
 	queue_free()
 
 
+# Closing task
 func _on_CloseButtonUnfinineshed_pressed():
 	get_node("/root/Level 1/Player").canWalk = true
 	get_node("/root/Level 1/Cenário 2").taskOcorring = false
 	queue_free()
 
 
+# Closing introduction screen
 func _on_ButtonCloseIntroduction_pressed():
 	$IntroductionScreen.queue_free()
